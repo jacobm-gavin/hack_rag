ef6577b2633d29b1ca51882448c651364ab1fa3c
jacobm-gavin <jacobm.gavin@outlook.com>
2024-10-12 18:47:37 -0400

    refactored

--- server/aux.py
+++ server/aux.py
@@ -2,6 +2,3 @@ from pydantic import BaseModel
 
 class Request(BaseModel):
     text: str
-    documentID: int
-    temperature: float | None = None
-    max_tokens: int | None = None
--- server/backend.py
+++ server/backend.py
@@ -1,5 +1,7 @@
 import fastapi
 from aux import Request
+from embedchain import App
+embedchain = App()
 
 app = fastapi.FastAPI()
 
@@ -7,11 +9,15 @@ app = fastapi.FastAPI()
 async def root():
     return {"message": "landing page"}
 
+@app.post('/embedpdf')
+async def embedpdf(request: Request):
+    embedchain.add(request.text, data_type='pdf_file')
+
+@app.post('/embedwebsite')
+async def embedwebsite(request: Request):
+    embedchain.add(request.text)
 
 @app.post("/request")
 async def request(request: Request):
-    return {"message": "request received", "request": request.dict()}
-
-
-
-app.run()
+    response = embedchain.query(request.text, citations=True)
+    return response
