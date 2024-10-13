5dcb839488a288bff0b750148c280fef3ab12f87
Nahsoff <130424768+Nahsoff@users.noreply.github.com>
2024-10-13 13:14:20 -0400

    Fixed server upload

--- server/ui/src/App.js
+++ server/ui/src/App.js
@@ -1,58 +1,73 @@
-import "./App.css";
-import { useState } from "react";
-import { GetData } from "./rag_response";
-import { answer } from "./rag_response";
-let final_question = "";
+import './App.css';
+import { useState } from 'react';
+import { GetData } from './rag_response';
+import { answer } from './rag_response';
+let final_question='';
+let final_context='';
 function App() {
-  const [in_question, setIn_question] = useState("");
-  const [rag_response, setRag_response] = useState("");
+  const [app_header, setApp_header]=useState('App-header');
+  const [prompt, setPrompt]=useState('Prompt');
+  const [question, setQuestion]=useState('Question');
+  const [submit, setSubmit]=useState('Submit');
+  const [css_answer, setCss_answer]=useState('Css-answer');
+  const [context_prompt, setContext_prompt]=useState('Context-prompt');
+  const [context_prov, setContext_prov]=useState('Context-prov');
+
+  const [in_question, setIn_question]=useState('');
+  const [in_context, setIn_context]=useState('');
+  const [rag_response, setRag_response] = useState('');
   const handleInput = (event) => {
     event.preventDefault();
     console.log(event.target.value);
     setIn_question(event.target.value);
-  };
-  const handleAnswer = async () => {
-    final_question = in_question;
+  }
+  const handleContextInput = (event) => {
+    event.preventDefault();
+    console.log(event.target.value);
+    setIn_context(event.target.value);
+  }
+  const handleAnswer = async() => {
+    
+    final_question=in_question;
+    final_context=in_context;
     await GetData();
-    setRag_response(answer);
-    console.log(rag_response + " Is the current value of rage_response");
-    let response = "";
+    let response = '';
     console.log(answer.length);
-    /*
     let i = 0;
-    const writing = setInterval(function () {
-      if (i < answer.length) {
-        console.log(answer.charAt(i));
-        response += answer.charAt(i);
-        setRag_response(response);
-        i++;
-      } else {
-        clearInterval(writing);
-      }
-    }, 150);*/
+    setApp_header('App-header-new');
+    setPrompt('Prompt-new');
+    setQuestion('Question-new');
+    setSubmit('Submit-new');
+    setCss_answer('Css-answer-new');
+    setContext_prompt('Context-prompt-new');
+    setContext_prov('Context-prov-new');
+    const writing = setInterval(function(){
+        if (i<answer.length){
+          console.log(answer.charAt(i));
+          response += answer.charAt(i);
+          setRag_response(response);
+          i++;}
+        else{
+          clearInterval(writing);}},150);
     // console.log(answer);
     // setRag_response(answer);
-  };
+    }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Your Question:</p>
-        <input
-          id="in_question"
-          value={in_question}
-          onChange={handleInput}
-        ></input>
-        <br></br>
-        <br></br>
-        <button onClick={handleAnswer}>Submit question</button>
-        <br></br>
-        <br></br>
-        <p>{rag_response}</p>
+      <header className={app_header}>
+        <p className={prompt}>Your Question:</p>
+        <input className={question} value={in_question} onChange={handleInput} placeholder='Enter your question here'></input><br></br>
+        <p className={context_prompt}>Your source:</p><br></br>
+        <input className={context_prov} value={in_context} onChange={handleContextInput} placeholder='Enter link here'></input>
+        <br></br><br></br>
+        <button className={submit}onClick={handleAnswer}>Submit question</button><br></br><br></br>
+        <p className={css_answer}>{rag_response}</p>
       </header>
     </div>
   );
 }
 console.log(final_question);
-export { final_question };
+export {final_question};
+export {final_context};
 export default App;
--- server/ui/src/rag_response.js
+++ server/ui/src/rag_response.js
@@ -1,31 +1,25 @@
-import { useState } from "react";
-import { final_question } from "./App.js";
-let answer = "";
-let context = "";
+import { useState } from 'react';
+import { final_question } from './App.js';
+import { final_context } from './App.js';
+let answer = '';
+let context = '';
 export async function GetData() {
-  const ip = "/request";
-  //answer = 'hello';
-  //let context = '';
-  try {
-    const response = await fetch(ip, {
-      method: "POST",
-      body: JSON.stringify({ text: final_question }),
-    });
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
+    const ip = '127.0.0.1';
+    answer='hello';
+    try {
+      const ctxresponse = await fetch(ip,{method:'POST', text:final_context});
+      const response = await fetch(ip,{method:'POST', text:final_question});
+      if (!response.ok || !ctxresponse.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      answer = json[0];
+      context = json[1];
+      
+    } catch (error) {
+        console.error(error.message);
     }
-
-    const json = await response.json();
-    //const arr = JSON.parse(json);
-    answer = json[0];
-    context = json[1];
-    console.log(answer);
-    return { answer: answer, context: context };
-  } catch (error) {
-    console.error(error.message);
   }
-}
-console.log(answer);
-export { answer };
-export { context };
-// export const context = getData.ctx
+export {answer};
+export {context};
